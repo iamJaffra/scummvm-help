ee356b392c171abadbe1ca489e2b4a10b87d3b35
Jaffra <selpahi@gmail.com>
2025-09-28 11:48:01 +0200

    Delete Engines.cs

--- Engines.cs
+++
@@ -1,9 +0,0 @@
-﻿public class Mohawk_Myst : ScummVM { }
-
-public class Mohawk_Riven : ScummVM { }
-
-public class SCI : ScummVM { }
-
-public class Sword : ScummVM { }
-
-public class Toon : ScummVM { }
